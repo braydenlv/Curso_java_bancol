54e9cd062a23de4d287342ee19a0fca8626bccc8
Brayden Lopera <blopera@bancolombia.com.co>
2024-09-24 03:53:34 -0500

    Se añade la practica 8 al repositorio

---
+++ BancaElectronica/src/main/java/co/com/bancolombia/entregable2/practica8/Cliente.java
@@ -0,0 +1,189 @@
+package co.com.bancolombia.entregable2.practica8;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class Cliente implements ServicioCuentas, Comparable<Cliente> {
+    private int numero;
+    private String nombre;
+    private Domicilio domicilio;
+    private String rfc;
+    private String telefono;
+    private ArrayList<Cuenta> cuentas;
+    private String fechaNacimiento;
+
+    public Cliente(Builder builder) {
+        this.numero = builder.numero;
+        this.nombre = builder.nombre;
+        this.domicilio = builder.domicilio;
+        this.rfc = builder.rfc;
+        this.telefono = builder.telefono;
+        this.cuentas =  new ArrayList<>();
+        this.fechaNacimiento = builder.fechaNacimiento;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Domicilio getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(Domicilio domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public ArrayList<Cuenta> getCuentas() {
+        return cuentas;
+    }
+
+    public void setCuentas(ArrayList<Cuenta> cuentas) {
+        this.cuentas = cuentas;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public static class Builder {
+        private final int numero;
+        private final String nombre;
+        private Domicilio domicilio;
+        private String rfc;
+        private String telefono;
+        private String fechaNacimiento;
+
+        Builder (int numero, String nombre){
+            this.numero = numero;
+            this.nombre = nombre;
+        }
+
+        Builder domicilio (Domicilio domicilio){
+            this.domicilio = domicilio;
+            return this;
+        }
+
+        Builder rfc (String rfc){
+            this.rfc = rfc;
+            return this;
+        }
+
+        Builder telefono (String telefono){
+            this.telefono = telefono;
+            return this;
+        }
+
+        Builder fechaNacimiento (String fechaNacimiento){
+            this.fechaNacimiento = fechaNacimiento;
+            return this;
+        }
+
+        Cliente build (){
+            return new Cliente(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "\n::::::::::::::::::::::::::::: Cliente " + numero + " :::::::::::::::::::::::::::::\n" +
+                "Nombre= " + nombre + "\n" +
+                "Domicilio= " + domicilio + "\n" +
+                "RFC= " + rfc + "\n" +
+                "Teléfono= " + telefono + "\n" +
+                "Fecha nacimiento= " + fechaNacimiento + "\n" +
+                "Cuentas= " + cuentas + "\n";
+    }
+
+    @Override
+    public boolean agregarCuenta(Cuenta cuenta) {
+        try {
+            this.cuentas.add(cuenta);
+            return true;
+        } catch (Exception e){
+            throw new RuntimeException("No se pudo agregar la cuenta " + cuenta + " al cliente " + getNombre() + ": " + e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean cancelarCuenta(int numeroCuenta) {
+        try {
+            Optional<Cuenta> cuentaAuxiliar = cuentas.stream().filter(cuenta -> cuenta.getNumero() == numeroCuenta).findFirst();
+            if (cuentaAuxiliar.isPresent()){
+                cuentaAuxiliar.ifPresent(cuenta -> cuenta.setFechaCancelacion("24-09-2024"));
+                return true;
+            }
+            return false;
+        } catch (Exception e){
+            throw new RuntimeException("No se pudo cancelar la cuenta " + numeroCuenta + " del cliente " + getNombre() + ": " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void abonarCuenta(int numeroCuenta, double abono) {
+        try {
+            cuentas.stream().filter(cuenta -> cuenta.getNumero() == numeroCuenta).findFirst()
+                    .ifPresent(cuenta -> {
+                        cuenta.setSaldo(cuenta.getSaldo() + abono);
+                    });
+        } catch (Exception e){
+            throw new RuntimeException("No se pudo encontrar la cuenta " + numeroCuenta + " del cliente " + getNombre() + ": " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void retirar(int numeroCuenta, double retiro) {
+        try {
+            cuentas.stream().filter(cuenta -> cuenta.getNumero() == numeroCuenta).findFirst()
+                    .ifPresent(cuenta -> {
+                        if (cuenta.getSaldo() >= retiro){
+                            cuenta.setSaldo(cuenta.getSaldo() - retiro);
+                        }
+                    });
+        } catch (Exception e){
+            throw new RuntimeException("No se pudo encontrar la cuenta " + numeroCuenta + " del cliente " + getNombre() + ": " + e.getMessage());
+        }
+    }
+
+    @Override
+    public ArrayList<Cuenta> obtenerCuentas() {
+        return this.cuentas;
+    }
+
+    @Override
+    public int compareTo(Cliente cliente) {
+        return Integer.compare(this.numero, cliente.getNumero());
+    }
+}
---
+++ BancaElectronica/src/main/java/co/com/bancolombia/entregable2/practica8/Main.java
@@ -0,0 +1,110 @@
+package co.com.bancolombia.entregable2.practica8;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /****** SE CREA EL BANCO BANCOLOMBIA ******/
+        Domicilio bancolombiaDomicilio = new Domicilio("Av. El Poblado", 31, "Medellin", "Antioquia", 462);
+        Banco bancolombia = new Banco("Bancolombia", bancolombiaDomicilio, "rfcBancolombia", "6045128080");
+
+        /****** CREACIÓN CLIENTE UNO ******/
+        Domicilio clienteUnoDomicilio = new Domicilio("La Central", 23, "Medellin", "Antioquia", 587);
+        Cliente clienteUno = new Cliente.Builder(1, "Brayden Lopera")
+                .domicilio(clienteUnoDomicilio)
+                .rfc("rfcClienteUno")
+                .telefono("6047895623")
+                .fechaNacimiento("02-05-1996")
+                .build();
+
+        /****** CREACIÓN CLIENTE DOS ******/
+        Domicilio clienteDosDomicilio = new Domicilio("Av. Santa Helena", 43, "Medellin", "Antioquia", 236);
+        Cliente clienteDos = new Cliente.Builder(2, "Xiomara Gomez")
+                .domicilio(clienteDosDomicilio)
+                .rfc("rfcClienteDos")
+                .telefono("6047854152")
+                .fechaNacimiento("28-07-2001")
+                .build();
+
+        /****** CREACIÓN CLIENTE TRES ******/
+        Domicilio clienteTresDomicilio = new Domicilio("Calle La Esperanza", 96, "Medellin", "Antioquia", 876);
+        Cliente clienteTres = new Cliente.Builder(3, "Estefania Vasquez")
+                .domicilio(clienteTresDomicilio)
+                .rfc("rfcClienteTres")
+                .telefono("6044734152")
+                .fechaNacimiento("07-07-2000")
+                .build();
+
+        String rutaDelArchivo = "src/main/java/co/com/bancolombia/entregable2/practica8/ListadoDeCuentas.txt";
+        List<String> informacionCuentas = leerArchivoDeCuentas(rutaDelArchivo);
+
+        for (String cuenta : informacionCuentas) {
+            String cuentaAuxiliar = cuenta.replace("[",",");
+            cuenta = cuentaAuxiliar.replace("]", "");
+            String[] partes = cuenta.split(",");
+            String tipoCuenta = partes[0].trim();
+            int numeroCuenta = Integer.parseInt(partes[1].trim());
+            String fechaApertura = partes[2].trim();
+            int saldo = Integer.parseInt(partes[3].trim());
+            int cargosDeLaCuenta = Integer.parseInt(partes[4].trim());
+            int numeroCliente = Integer.parseInt(partes[5].trim());
+
+            Cuenta cuentaCliente;
+
+            if(tipoCuenta.equals("CA")){
+                cuentaCliente = new CuentaDeAhorro(numeroCuenta,saldo,cargosDeLaCuenta);
+            } else {
+                cuentaCliente = new CuentaDeCheque(numeroCuenta,saldo,cargosDeLaCuenta);
+            }
+            cuentaCliente.setFechaApertura(fechaApertura);
+
+            switch (numeroCliente){
+                case 1:
+                    clienteUno.agregarCuenta(cuentaCliente);
+                    break;
+                case 2:
+                    clienteDos.agregarCuenta(cuentaCliente);
+                    break;
+                case 3:
+                    clienteTres.agregarCuenta(cuentaCliente);
+                    break;
+                default:
+                    throw new RuntimeException("No se encontró el cliente");
+            }
+
+        }
+
+        System.out.println(clienteUno);
+        System.out.println(clienteDos);
+        System.out.println(clienteTres);
+
+        clienteUno.abonarCuenta(1234, 3333);
+        clienteDos.retirar(123456, 878);
+
+        /****** SE AÑADEN CLIENTES A BANCOLOMBIA *******/
+        bancolombia.agregarCliente(clienteUno);
+        bancolombia.agregarCliente(clienteDos);
+        System.out.println(" ::::::::::::::::::::::::::::: CLIENTES :::::::::::::::::::::::::::::");
+        System.out.println(bancolombia.obtenerClientes());
+
+
+    }
+
+    public static List<String> leerArchivoDeCuentas(String nombreArchivo) {
+        List<String> cuentas = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            while ((linea = bufferedReader.readLine()) != null) {
+                cuentas.add(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+        return cuentas;
+    }
+}
